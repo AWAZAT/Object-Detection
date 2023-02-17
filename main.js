e25555838181ecfd38f0a2a1dc20fb4ac83c8ac3
AWAZAT <86447771+AWAZAT@users.noreply.github.com>
2023-02-16 20:02:48 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function bedroom()
+{
+  window.location = "bedroom.html";
+}
+
+function bottles()
+{
+  window.location = "bottles.html";
+}
+function desk()
+{
+  window.location = "desk.html";
+}
+
+function fruitbowl()
+{
+  window.location = "fruit_bowl.html";
+}
+
+function tvac()
+{
+  window.location = "tv+ac.html";
+}
